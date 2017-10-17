7835d56db69ce25ababab328e559888e4b826fe3
Dmitry Volodin <dmvolod@gmail.com>
2017-10-17 12:06:22 +0300

    Add possibility to run in mixed envs

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ cd grpc-sb-camel-client folder
 
 ./mvnw clean package spring-boot:run -Drun.profile=dev
 
-### How to run on OpenShift (minishift) instance
+### How to run both (all in one) server and client on OpenShift (minishift) instance
 
 1. minishift start
 
@@ -25,3 +25,20 @@ cd grpc-sb-camel-client folder
 5. cd grpc-sb-camel-client folder
 
 ./mvnw clean package fabric8:deploy
+
+### How to run server on OpenShift (minishift) instance and client on other host
+Please note, that remote service routing on OpenShift (minishift) supports TLS passthrough for HTTP/2  
+
+1. minishift start
+
+2. oc login 
+
+3. oc new-project rh-forum-camel-demo
+
+4. cd grpc-sb-camel-server folder
+
+./mvnw clean package fabric8:deploy
+
+5. cd grpc-sb-camel-client folder
+
+./mvnw clean package spring-boot:run -Drun.profiles=ose-remote
--- grpc-sb-camel-client/src/main/java/ru/redhat/forum/demo/GrpcClientRoute.java
+++ grpc-sb-camel-client/src/main/java/ru/redhat/forum/demo/GrpcClientRoute.java
@@ -2,30 +2,36 @@ package ru.redhat.forum.demo;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Random;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import ru.redhat.forum.demo.gen.TestRequest;
-import ru.redhat.forum.demo.gen.TestResponse;
 
 @Component
 public class GrpcClientRoute extends RouteBuilder {
+    
+    @Autowired
+    private Environment environment;
 
     @Override
     public void configure() throws Exception {
-        /*
-        from("timer:foo?period=2000")
-            .bean(new GrpcMessageBuilder(), "buildTestRequest")
-            .to("grpc://{{hello-grpc-service}}/ru.redhat.forum.demo.gen.DemoService?method=TestCall")            
-            .log("Body: ${body}");
-        */
-        from("timer:foo?period=2000")
-            .bean(new GrpcMessageBuilder(), "buildTestRequest")
-            .to("grpc://{{hello-grpc-service}}/ru.redhat.forum.demo.gen.DemoService?method=TestCall&negotiationType=TLS&keyCertChainResource=classpath:certs/client.pem&" 
-            + "keyResource=classpath:certs/client.key&trustCertCollectionResource=classpath:certs/ca.pem")
-            .log("Body: ${body}");
+        if (Arrays.asList(environment.getActiveProfiles()).contains("ose-remote")) {
+            from("timer:foo?period=2000")
+                .bean(new GrpcMessageBuilder(), "buildTestRequest")
+                .to("grpc://{{hello-grpc-service}}/ru.redhat.forum.demo.gen.DemoService?method=TestCall&negotiationType=TLS&keyCertChainResource=classpath:certs/client.pem&" 
+                    + "keyResource=classpath:certs/client.key&trustCertCollectionResource=classpath:certs/ca.pem")
+                .log("Body: ${body}");
+        } else {
+            from("timer:foo?period=2000")
+                .bean(new GrpcMessageBuilder(), "buildTestRequest")
+                .to("grpc://{{hello-grpc-service}}/ru.redhat.forum.demo.gen.DemoService?method=TestCall")            
+                .log("Body: ${body}");
+        }
     }
     
     public class GrpcMessageBuilder {
